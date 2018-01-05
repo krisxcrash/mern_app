3fbe47e05a20b7f497f1f94b5448eec6f5935420
Kristine Martin <kristinenicolemartin@gmail.com>
2018-01-04 16:38:56 -0800

    commented out about section for now

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ import Module1 from './components/Module1.js';
 import Module2 from './components/Module2.js';
 import Module3 from './components/Module3.js';
 import ModuleBlock from './components/ModuleBlock.js';
-import About from "./components/About.js";
+// import About from "./components/About.js";
 import Footer from "./components/Footer.js";
 
 class App extends Component {
